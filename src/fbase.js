9112400959304ce4df5b993f19d52c881da6e4eb
yoogle <dmltjs851@gmail.com>
2023-01-21 01:30:26 +0900

    complete: 2.1 Login Form part One

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,5 @@
-import * as firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -10,5 +10,5 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 
-export default firebase.initializeApp(firebaseConfig);
-export const authService = firebase.getApp;
+const app = initializeApp(firebaseConfig);
+export const authService = getAuth();
