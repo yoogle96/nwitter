40e3361c76aee1914432ac39a7c3f90454060d98
yoogle <dmltjs851@gmail.com>
2023-01-21 20:34:25 +0900

    complete: 2.4. Log In

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {target: {name, value}} = event;
         if (name == "email") {
@@ -28,20 +29,25 @@ const Auth = () => {
             }
             console.log(data)
         } catch(error) {
-            console.log(error)
-        }
-        
+            setError(error.message)
+        }        
     }
+    const toggleAccount = () => setNewAccount((prev) => !prev)
+
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
+                <button name="google">Continue with Google</button>
+                <button name="github">Continue with Github</button>
             </div>
         </div>
     )
