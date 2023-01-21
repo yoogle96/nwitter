f1b817544abb20f2ee707d130ce21a693679427d
yoogle <dmltjs851@gmail.com>
2023-01-22 00:04:02 +0900

    complete: 2.6 Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,21 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation/>}
             <Routes>
-                {isLoggedIn ? <Route path="/" element={<Home />}/> : <Route path="/" element={<Auth />}/>}
+                {isLoggedIn ? (
+                    <>
+                        <Route path="/" element={<Home />}/>
+                        <Route path="/profile" element={<Profile />}/>
+                    </>
+                ) : 
+                <Route path="/" element={<Auth />}/>}
             </Routes>
         </Router>
     )
