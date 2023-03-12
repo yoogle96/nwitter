3dc5857a567eb43177f7efc741b7f4570ccab3c5
yoogle <dmltjs851@gmail.com>
2023-03-12 12:02:10 +0900

    complete: get my own nweets, update profile

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,12 +7,12 @@ import Navigation from "components/Navigation";
 const AppRouter = ({ isLoggedIn, userObj }) => {
     return (
         <Router>
-            {isLoggedIn && <Navigation/>}
+            {isLoggedIn && <Navigation userObj={userObj}/>}
             <Routes>
                 {isLoggedIn ? (
                     <>
                         <Route path="/" element={<Home userObj={userObj}/>}/>
-                        <Route path="/profile" element={<Profile />}/>
+                        <Route path="/profile" element={<Profile userObj={userObj}/>}/>
                     </>
                 ) : 
                 <Route path="/" element={<Auth />}/>}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,60 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, collectionService, dbService } from "fbase";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { query, where, getDocs, orderBy } from "firebase/firestore";
+import { useEffect } from "react";
+import { updateProfile } from "firebase/auth";
 
-export default () => {
+export default ({ userObj }) => {
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNewDisplayName(value);
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.up
+            updateProfile(userObj, {
+                displayName: newDisplayName,
+            })
+        }
+    }
+
     const navigate = useNavigate();
     const onLogOutClick = () => {
         authService.signOut();
         navigate("/");
-    }
+    };
+
+    const getMyNweets = async () => {
+        const q = query(
+            collectionService(dbService, "nweets"),
+            where("creatorId", "==", userObj.uid),
+            orderBy("createdAt")
+        );
+        // console.log(q)
+        const querySnapshot = await getDocs(q);
+        // console.log(querySnapshot)
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, "=>", doc.data());
+        });
+    };
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
+
     return (
-    <>
-        <button onClick={onLogOutClick}>Log Out</button>
-    </>
+        <>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} type="text" placeholder="Dispaly name" value={newDisplayName}/>
+                <input type="submit" value="Update Profile" />
+            </form>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
     );
-}
+};
